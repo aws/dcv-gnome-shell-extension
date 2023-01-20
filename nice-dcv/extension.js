3b76b2149a59a51b376d2aafffc6bb0ea65eeba4
Alessandro Bono <alessandro.bono369@gmail.com>
2023-01-20 15:12:38 +0100

    Adapt to upstream changes
    See https://gitlab.gnome.org/GNOME/gnome-shell/-/merge_requests/2399

--- nice-dcv/extension.js
+++ nice-dcv/extension.js
@@ -38,107 +38,40 @@ let DcvShellUserVerifier = class DcvShellUserVerifier extends GdmUtil.ShellUserV
     constructor(client, params) {
         super(client, params);
 
-        let dcvCredentialManager = Dcv.getDcvCredentialsManager();
-        this._credentialManagers[Dcv.SERVICE_NAME] = dcvCredentialManager;
-        if (dcvCredentialManager.token) {
-            this._onCredentialManagerAuthenticated(dcvCredentialManager,
-                                                   dcvCredentialManager.token);
-        }
-
-        dcvCredentialManager.connectObject('user-authenticated',
-                                           this._onCredentialManagerAuthenticated.bind(this),
-                                           this);
+        this.addCredentialManager(Dcv.SERVICE_NAME, Dcv.getDcvCredentialsManager());
     }
 };
-
-let DcvAuthPrompt = GObject.registerClass(class DcvAuthPrompt extends GdmAuthPrompt.AuthPrompt {
-    reset() {
-        let oldStatus = this.verificationStatus;
-        this.verificationStatus = AuthPromptStatus.NOT_VERIFYING;
-        this.cancelButton.reactive = this._hasCancelButton;
-        this.cancelButton.can_focus = this._hasCancelButton;
-        this._preemptiveAnswer = null;
-
-        if (this._userVerifier)
-            this._userVerifier.cancel();
-
-        this._queryingService = null;
-        this.clear();
-        this._message.opacity = 0;
-        this.setUser(null);
-        this._updateEntry(true);
-        this.stopSpinning();
-
-        if (oldStatus == AuthPromptStatus.VERIFICATION_FAILED)
-            this.emit('failed');
-        else if (oldStatus === AuthPromptStatus.VERIFICATION_CANCELLED)
-            this.emit('cancelled');
-
-        let beginRequestType;
-
-        if (this._mode == AuthPromptMode.UNLOCK_ONLY) {
-            // The user is constant at the unlock screen, so it will immediately
-            // respond to the request with the username
-            if (oldStatus === AuthPromptStatus.VERIFICATION_CANCELLED)
-                return;
-            beginRequestType = BeginRequestType.PROVIDE_USERNAME;
-        } else if (this._userVerifier.serviceIsForeground(OVirt.SERVICE_NAME) ||
-                   this._userVerifier.serviceIsForeground(Vmware.SERVICE_NAME) ||
-                   this._userVerifier.serviceIsForeground(Dcv.SERVICE_NAME) ||
-                   this._userVerifier.serviceIsForeground(GdmUtil.SMARTCARD_SERVICE_NAME)) {
-            // We don't need to know the username if the user preempted the login screen
-            // with a smartcard or with preauthenticated oVirt credentials
-            beginRequestType = BeginRequestType.DONT_PROVIDE_USERNAME;
-        } else if (oldStatus === AuthPromptStatus.VERIFICATION_IN_PROGRESS) {
-            // We're going back to retry with current user
-            beginRequestType = BeginRequestType.REUSE_USERNAME;
-        } else {
-            // In all other cases, we should get the username up front.
-            beginRequestType = BeginRequestType.PROVIDE_USERNAME;
-        }
-
-        this.emit('reset', beginRequestType);
-    }
-});
 
 class Extension {
     constructor() {
         this._remoteAccessController = global.backend.get_remote_access_controller();
 
         this._originalShellUserVerifier = GdmUtil.ShellUserVerifier;
-        this._originalAuthPrompt = GdmAuthPrompt.AuthPrompt;
         this._originalInhibit = this._remoteAccessController.inhibit_remote_access;
         this._originalUninhibit = this._remoteAccessController.uninhibit_remote_access;
     }
 
     enable() {
-        let manager = Dcv.getDcvCredentialsManager();
         GdmUtil.ShellUserVerifier = DcvShellUserVerifier;
-        GdmAuthPrompt.AuthPrompt = DcvAuthPrompt;
-        manager.connectObject('user-authenticated',
-                              () => {
-                                  if (!Main.screenShield)
-                                      return;
-
-                                  if (Main.screenShield._isLocked)
-                                      Main.screenShield._activateDialog();
-                              },
-                              this);
 
         this._remoteAccessController.inhibit_remote_access = () => {};
         this._remoteAccessController.uninhibit_remote_access = () => {};
+
+        if (Main.screenShield) {
+            Main.screenShield.addCredentialManager(Dcv.SERVICE_NAME, Dcv.getDcvCredentialsManager());
+        }
 
         log(`${Me.metadata.name} enabled`);
     }
 
     disable() {
-        let manager = Dcv.getDcvCredentialsManager();
-        manager.disconnectObject(this);
         GdmUtil.ShellUserVerifier = this._originalShellUserVerifier;
-        GdmAuthPrompt.AuthPrompt = this._originalAuthPrompt;
-
         this._remoteAccessController.inhibit_remote_access = this._originalInhibit;
         this._remoteAccessController.uninhibit_remote_access = this._originalUninhibit;
+
+        if (Main.screenShield) {
+            Main.screenShield.removeCredentialManager(Dcv.SERVICE_NAME);
+        }
 
         log(`${Me.metadata.name} disabled`);
     }
